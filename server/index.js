02d4b5574ff1e10eb83f4eea41b39ce0e46810fb
root <marcusbarnesdev@gmail.com>
2020-05-20 23:46:59 -0700

    add update feature

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const db = require('./db/index.js');
 
 const app = express();
 const PORT = 3002;
-const jsonParser = bodyParser.json();
 
 app.listen(PORT, () => {
   console.log(`Now listening on port ${PORT}!`);
@@ -13,6 +12,8 @@ app.listen(PORT, () => {
 
 app.use(express.static('public'));
 app.use('/games/:gameId', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/reviews/:gameId', (req, res) => {
   db.Review.find({ gameId: req.params.gameId }, (err, result) => {
@@ -75,6 +76,19 @@ app.get('/review/:id', (req, res) => {
       res.end();
     } else {
       res.send(data);
+      res.end();
+    }
+  });
+});
+
+app.put('/review/:id', (req, res) => {
+  const id = String(req.params.id);
+  db.Review.findOneAndUpdate({ _id: id }, req.body, (err) => {
+    if (err) {
+      res.status(500);
+      res.end();
+    } else {
+      res.status(200);
       res.end();
     }
   });
